15c4a6076f3b460f7e5447f074652c125ed1a239
johnRojas08 <johnrojas0808@gmail.com>
2024-11-17 19:17:54 +0100

    Añadiendo funcionalidad para recibiri y validar el tamaño

--- src/RojasJohnEjercicio2.java
+++ src/RojasJohnEjercicio2.java
@@ -5,15 +5,25 @@ public class RojasJohnEjercicio2 {
 
         // Declarar variables necesarias
         Scanner scanner = new Scanner(System.in);
-        char pattern;
+        char patron;
+        int tamaño;
 
         // Solicitar el patrón al usuario
         System.out.print("Introduce el patrón para las casillas: ");
-        pattern = scanner.next().charAt(0);
+        patron = scanner.next().charAt(0);
+
+        // Solicitar el tamaño al usuario
+        do {
+            System.out.print("Introduce el tamaño de las casillas (1-15): ");
+            tamaño = scanner.nextInt();
+            if (tamaño < 1 || tamaño > 15) {
+                System.out.println("Introduce un tamaño entre 1 y 15.");
+            }
+        } while (tamaño < 1 || tamaño > 15);
 
         // Imprimir el patrón ingresado (para probar este paso)
-        System.out.println("El patrón elegido es: " + pattern);
+        System.out.println("El patrón elegido es: " + patron);
 
         scanner.close();
+        }
     }
-}
